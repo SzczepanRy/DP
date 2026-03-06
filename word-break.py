6dc405de21f6c62d365ca1f732951389c5c5cfce
unknown <szep.rydzewski@gmail.com>
2026-03-06 17:42:52 +0100

    add

--- word-break.py
+++ word-break.py
@@ -1,37 +1,20 @@
+
+
 class Solution:
     def wordBreak(self, s, wordDict ) :
+        n = len(s)
 
-        exist= []
-        res= 0
+        dp= [False] *(n+1)
+        dp[0] = True
 
-        def wordcheck(s ,sea):
-            n = len(s)
-            for i in range(len(sea) - n +1):
-                if sea[i:i+n] == s:
-                    t1= sea[:i+1]
-                    t2= sea[i+n-1:]
-                    sea=t1+t2
-                    return (True, sea)
-            return (False, sea)
+        # l is the length of the string beeing constdered
+        for l in range(1,n+1 ):
+            for i in range(l):
+                if dp[i] and s[i:l] in wordDict:
+                    dp[l]= True
+                    break
+        return dp[len(s)]
 
-        for st in wordDict:
-            print(s)
-            if st in exist:
-                res +=1
-                continue
-            else:
-                vals =wordcheck(st, s)
-                if vals[0]:
-                    s= vals[1]
-                    res+=1
-                    exist.append(st)
-                else:
-                    return False
-
-        print(res, exist)
-        if res == len(wordDict):
-            return True
-        return False
 
 
 s =Solution()
